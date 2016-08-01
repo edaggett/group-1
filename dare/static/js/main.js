f440ea744c5efb61660f37aa3556d43c920237d6
jorgrav <cssi2016@CSSIs-MacBook-Air.local>
2016-08-01 15:38:01 -0500

    redirect

--- dare/static/js/main.js
+++ dare/static/js/main.js
@@ -1,11 +1,14 @@
-function redirect_to_advice() {
+function redirect_to_dare() {
 	window.location.replace("/dare");
 }
 
+function you_submitted() {
+	$('radio').click(alert('You have submitted'));
+}
 
 function setup() {
 	//alert("javascript is here!");
-    $("#kind").click(redirect_to_advice);
+    $("#kind").click(redirect_to_dare);
 }
 
 $(document).ready(setup)
