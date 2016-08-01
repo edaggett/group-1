4b9bbba9e5624691fdf9754d02d4b66e39aa6999
riyau98 <cssi2016@CSSIs-MacBook-Air.local>
2016-08-01 14:51:05 -0500

    login stuff works

--- dare/main.py
+++ dare/main.py
@@ -24,22 +24,22 @@ env=jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-       template=env.get_template("main.html")
+        template=env.get_template("main.html")
        
-       self.response.write(template.render())
+        user = users.get_current_user()
+        data={}
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+            (user.nickname(), users.create_logout_url('/')))
+            data['signed_in'] = True
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+            users.create_login_url('/'))
+            data["signed_in"]=False
 
-class LogInHandler():
-	def get(self):
-		user = users.get_current_user()
-		if user:
-			greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
-			(user.nickname(), users.create_logout_url('/')))
-		else:
-			greeting = ('<a href="%s">Sign in or register</a>.' %
-			users.create_login_url('/'))
 
-		self.response.out.write('<html><body>%s</body></html>' % greeting)
-
+        
+        self.response.write(template.render(data))
 
 class DareHandler(webapp2.RequestHandler):
     def get(self):
@@ -56,7 +56,7 @@ class DareHandler(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler), ("/dare", DareHandler)
+    ('/', MainHandler), ("/dare", DareHandler), 
 
 
 
