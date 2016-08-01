a4ea73867520fcd2a70177352d02d6983b8381dc
riyau98 <cssi2016@CSSIs-MacBook-Air.local>
2016-08-01 10:58:00 -0500

    updated python

--- dare/main.py
+++ dare/main.py
@@ -17,11 +17,12 @@
 import webapp2
 import jinja2
 import random
+env=jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
-
+       template=env.get_template("main.html")
+       self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
