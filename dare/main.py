210e753d263961d08e8b800d8b6377158e17aef2
riyau98 <cssi2016@CSSIs-MacBook-Air.local>
2016-08-01 14:54:02 -0500

    hi

--- dare/main.py
+++ dare/main.py
@@ -23,7 +23,7 @@ from dares import Dares
 env=jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
 
 class MainHandler(webapp2.RequestHandler):
-<<<<<<< HEAD
+
     def get(self):
         template=env.get_template("main.html")
        
@@ -37,22 +37,6 @@ class MainHandler(webapp2.RequestHandler):
             greeting = ('<a href="%s">Sign in or register</a>.' %
             users.create_login_url('/'))
             data["signed_in"]=False
-
-=======
-	def get(self):
-		template=env.get_template("main.html")
-		
-		self.response.write(template.render())
-		user = users.get_current_user()
-		if user:
-			greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
-			(user.nickname(), users.create_logout_url('/')))
-		else:
-			greeting = ('<a href="%s">Sign in or register</a>.' %
-			users.create_login_url('/'))
-
-		self.response.out.write('<html><body>%s</body></html>' % greeting)
->>>>>>> origin/master
 
         
         self.response.write(template.render(data))
@@ -73,19 +57,7 @@ class DareHandler(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-<<<<<<< HEAD
-    ('/', MainHandler), ("/dare", DareHandler), 
 
-
-
-
-
-
-
-
-
-=======
-	('/', MainHandler), ("/dare", DareHandler)
->>>>>>> origin/master
+    ('/', MainHandler), ("/dare", DareHandler),
 
 ], debug=True)
