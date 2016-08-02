f4681a0953c5065466ef5851781b86dbe04a9a16
edaggett <frbarcamf16@gmail.com>
2016-08-02 15:44:30 -0500

    Changes

--- dare/main.py
+++ dare/main.py
@@ -28,77 +28,79 @@ from google.appengine.ext import ndb
 env=jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
 
 def findUser (user):
-    find_dare_users=DareUsers.query().filter(DareUsers.user_id==user.user_id()).get()
-    if find_dare_users==None:
-        
-        current_user=users.get_current_user()
-        find_dare_users=DareUsers(user_id=current_user.user_id())
-        find_dare_users.put()
-    return find_dare_users
+	find_dare_users=DareUsers.query().filter(DareUsers.user_id==user.user_id()).get()
+	if find_dare_users==None:
+		
+		current_user=users.get_current_user()
+		find_dare_users=DareUsers(user_id=current_user.user_id())
+		find_dare_users.put()
+	return find_dare_users
 
 
 
 class Dares(ndb.Model):
-    dare_number=ndb.IntegerProperty(required=False)
-    dare=ndb.StringProperty(required=True, indexed=True)
+	dare_number=ndb.IntegerProperty(required=False)
+	dare=ndb.StringProperty(required=True, indexed=True)
 
 
 class MainHandler(webapp2.RequestHandler):
 
-    def get(self):
-        template=env.get_template("main.html")
-       
-        user = users.get_current_user()
-        
-        if user:
-            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
-            (user.nickname(), users.create_logout_url('/')))
-           # data["signed_in"]=True
-        else:
-            greeting = ('<a href="%s">Sign in or register</a>.' %
-            users.create_login_url('/'))
-            
-            #data["signed_in"]=False
+	def get(self):
+		template=env.get_template("main.html")
+	   
+		user = users.get_current_user()
+		
+		if user:
+			greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+			(user.nickname(), users.create_logout_url('/')))
+			# data["signed_in"]=True
+		else:
+			greeting = ('<a href="%s">Sign in or register</a>.' %
+			users.create_login_url('/'))
+			
+			#data["signed_in"]=False
 
-        data = {"LogIn" : greeting}
-        
+		data = {"LogIn" : greeting}
+		
 
-        self.response.write(template.render(data))
+		self.response.write(template.render(data))
 
 
 
 class DareHandler(webapp2.RequestHandler):
 	def get(self):
-	   template=env.get_template("dare.html")
-		
-	   dare_query=Dares.query()
-	   dare_results=dare_query.fetch()
-	   dare_result=dare_results[random.randint(0,len (dare_results)-1)]
+		template=env.get_template("dare.html")
+			
+		dare_query=Dares.query()
+		dare_results=dare_query.fetch()
+		dare_result=dare_results[random.randint(0,len (dare_results)-1)]
 
-	   dare={}
-	   dare["number"]=dare_result.dare_number
-	   dare["dare"]=dare_result.dare
-	   self.response.write(template.render(dare))
+		dare={}
+		dare["number"]=dare_result.dare_number
+		dare["dare"]=dare_result.dare
+		self.response.write(template.render(dare))
 
 class UserDare(webapp2.RequestHandler):
-    def post(self):
-        user_dare=self.request.get("daredare")
-        d=Dares(dare=user_dare)
-        d.put()
-        template=env.get_template("main.html")
-        self.response.write(template.render())
+	def post(self):
+		user_dare=self.request.get("daredare")
+		d=Dares(dare=user_dare)
+		d.put()
+		template=env.get_template("main.html")
+		self.response.write(template.render())
 
 class DareCompleted (webapp2.RequestHandler):
-    def post (self):
-        if (self.request.get("completed_dare"))=="True":
-             user=users.get_current_user()
-             dare_completed_user=findUser(user)
-             dare_completed_user.points+=1
-             dare_completed_user.put()
-        self.redirect("/")
+	def post (self):
+		if (self.request.get("completed_dare"))=="True":
+
+			user=users.get_current_user()
+			dare_completed_user=findUser(user)
+			dare_completed_user.points+=1
+			dare_completed_user.put()
+
+		self.redirect("/")
 
 
-        
+		
 app = webapp2.WSGIApplication([
 ('/userdare', UserDare),
-     ("/dare", DareHandler), ('/', MainHandler), ("/darecompleted", DareCompleted)], debug=True)
+	 ("/dare", DareHandler), ('/', MainHandler), ("/darecompleted", DareCompleted)], debug=True)
