6a29a2c29bd72975014c223ec8298b9cd78dea27
riyau98 <cssi2016@CSSIs-MacBook-Air.local>
2016-08-02 11:27:32 -0500

    change back

--- dare/main.py
+++ dare/main.py
@@ -47,20 +47,6 @@ class MainHandler(webapp2.RequestHandler):
             #data["signed_in"]=False
 
         data = {"LogIn" : greeting}
-        
-
-        user = users.get_current_user()
-        if user:
-            nickname = user.nickname()
-            logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                nickname, logout_url)
-        else:
-            login_url = users.create_login_url('/')
-            greeting = '<a href="{}">Sign in</a>'.format(login_url)
-
-        self.response.write(
-            '<html><body>{}</body></html>'.format(greeting))
         self.response.write(template.render(data))
 
 
