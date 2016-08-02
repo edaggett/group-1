0c1fb0feef8690215e0c6daf5988015a7a856298
riyau98 <cssi2016@CSSIs-MacBook-Air.local>
2016-08-02 15:14:32 -0500

    everything works

--- dare/static/js/main.js
+++ dare/static/js/main.js
@@ -9,6 +9,12 @@ function redirect_to_main() {
 function redirect_to_user() {
 	window.location.replace("/user");
 }
+function submit_alert(){
+		if ($("#didDare").prop("checked")){
+    	window.alert("You completed the dare! Your points have been recorded")
+    	}
+    	else{
+    	window.alert("Go try out the dare and spread a little kindness!")}}
 
 function setup() {
 
@@ -16,13 +22,9 @@ function setup() {
    
 
     $('#submissions').click(redirect_to_main);
-  
-// if {
-//     	window.alert("You completed the dare! Your points have been recorded")
-//     }
-//     else{
-//     	window.alert("Go try out the dare and spread a little kindness!")
-//     }
+  	$("#dareForm").submit(submit_alert);
+		
+     
     $("#user_dare").click(redirect_to_main);
 
 }
