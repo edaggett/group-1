3e819f8fd24549faa1229453652c26273ae174ec
riyau98 <cssi2016@CSSIs-MacBook-Air.local>
2016-08-02 10:07:10 -0500

    more js

--- dare/static/js/main.js
+++ dare/static/js/main.js
@@ -11,18 +11,16 @@ function redirect_to_user() {
 }
 
 function setup() {
-<<<<<<< HEAD
-	$("#kind").click(redirect_to_dare);
-    $('#submissions').click(redirect_main);
-=======
-	
-    $("#kind").click(redirect_to_dare);
 
+    $("#kind").click(redirect_to_dare);
+    if (#submissions==True):
+    	window.alert("You completed the dare! Your points have been recorded")
+    else:
+    	window.alert("Go try out the dare and spread a little kindness!")
     $('#submissions').click(redirect_to_main);
 
     $("#user_dare").click(redirect_to_main);
 
->>>>>>> origin/master
 }
 
 $(document).ready(setup)
